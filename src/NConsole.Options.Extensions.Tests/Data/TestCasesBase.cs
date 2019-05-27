59b3d1051b17b677ff94238eeada67f36dcb90be
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-27 11:33:18 -0400

    added test cases base class

---
+++ src/NConsole.Options.Extensions.Tests/Data/TestCasesBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace NConsole.Options
+{
+    /// <inheritdoc />
+    internal abstract class TestCasesBase : IEnumerable<object[]>
+    {
+        protected abstract IEnumerable<object[]> Cases { get; }
+
+        public IEnumerator<object[]> GetEnumerator() => Cases.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
