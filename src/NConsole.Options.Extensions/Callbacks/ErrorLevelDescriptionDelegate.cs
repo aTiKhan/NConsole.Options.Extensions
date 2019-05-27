0bf24b7c2edc3d56f3919b04adb67a864163082e
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-27 15:33:28 -0400

    added error level delegates

---
+++ src/NConsole.Options.Extensions/Callbacks/ErrorLevelDescriptionDelegate.cs
@@ -0,0 +1,8 @@
+﻿namespace NConsole.Options
+{
+    /// <summary>
+    /// Provides a Callback opportunity to furnish an Error Level Description.
+    /// </summary>
+    /// <returns></returns>
+    public delegate string ErrorLevelDescriptionDelegate();
+}
---
+++ src/NConsole.Options.Extensions/Callbacks/ErrorLevelFilterDelegate.cs
@@ -0,0 +1,9 @@
+﻿namespace NConsole.Options
+{
+
+    /// <summary>
+    /// Provides a Callback opportunity to furnish an Error Level Filter.
+    /// </summary>
+    /// <returns></returns>
+    public delegate bool ErrorLevelFilterDelegate();
+}
