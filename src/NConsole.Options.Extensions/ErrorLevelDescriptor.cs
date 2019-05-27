bad72a4652952d6d67744d69f5017a87b4087b1c
Michael W Powell <mwpowellhtx@gmail.com>
2019-05-27 15:34:15 -0400

    added error level descriptor

---
+++ src/NConsole.Options.Extensions/ErrorLevelDescriptor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace NConsole.Options
+{
+    public class ErrorLevelDescriptor
+    {
+        /// <summary>
+        /// Gets the ErrorLevel.
+        /// </summary>
+        public int ErrorLevel { get; internal set; }
+
+        private ErrorLevelFilterDelegate FilterCallback { get; }
+
+        /// <summary>
+        /// Implicitly Converts the <paramref name="descriptor"/> for evaluation purposes.
+        /// </summary>
+        /// <param name="descriptor"></param>
+        public static implicit operator bool(ErrorLevelDescriptor descriptor) => descriptor.FilterCallback.Invoke();
+
+        internal static ErrorLevelFilterDelegate DefaultFilterCallback => () => true;
+
+        private ErrorLevelDescriptionDelegate DescriptionCallback { get; }
+
+        internal static ErrorLevelDescriptionDelegate DefaultDescriptionCallback => () => null;
+
+        /// <summary>
+        /// Gets the Description.
+        /// </summary>
+        public string Description => DescriptionCallback.Invoke();
+
+        internal ErrorLevelDescriptor(int errorLevel, ErrorLevelFilterDelegate filterCallback
+            , ErrorLevelDescriptionDelegate descriptionCallback)
+        {
+            ErrorLevel = errorLevel;
+            FilterCallback = filterCallback;
+            DescriptionCallback = descriptionCallback;
+        }
+    }
+}
